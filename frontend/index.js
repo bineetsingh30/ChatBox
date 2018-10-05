6e35488943ecb8daa46c1c62670e2e6e1912e163
bineetsingh30 <bineetsingh30@gmail.com>
2018-10-05 20:04:16 +0530

    App is working

--- frontend/index.js
+++ frontend/index.js
@@ -1,26 +1,76 @@
 let socket = io()
-let login = $('#login')
-$(function(){
-    let submit = $('#submit')
-    console.log(submit)
-    submit.click(function(){
-        let username = $('#username').val()
-        if(username == undefined){
-            alert('Username cannot be empty')
-        } else {
-            socket.emit('login', {username: username})
-            login.hide()
-            
-        }    
-    })
+var roomidglobal = null;
 
+socket.on('roomcreated', function(data){
+    $('#login').hide()
+    $('#messagebox').show()
+    $('#messagebox').prepend(`Room id: ${data.id}`)
+    socket.emit('send_msg', {"message": `Joined`, id: data.id} )
+    roomidglobal = data.id;
+})
+
+socket.on("noroomexists", function(){
+    alert('Room details incorrect')
+})
+
+socket.on('rcv_msg', function(data){
+    let messagelist = $('#message-list')
+    messagelist.append(`<li>${data.name}: ${data.message}</li>`)
+})
+
+$(function(){
     let enter = $('#enter')
     let create  = $('#create')
+    
     create.click(function(){
         enter.hide()
         create.hide()
-        
+        let creatediv = $('#creatediv')
+        creatediv.show()
     })
 
+    enter.click(function(){
+        enter.hide()
+        create.hide()
+        let enterdiv = $('#enterdiv')
+        enterdiv.show()
+    })
+
+    let createroom = $('#createroom')
+    let enterroom = $('#enterroom')
+
+    createroom.click(function(){
+        let username = $('#cusername').val()
+        let password = $('#cpassword').val()
+
+        if(username == "" || password == ""){
+            alert("Field cannot be empty")
+        } else {
+            socket.emit('createroom', {"name": username, "password": password})
+        }
+    })
+
+
+    enterroom.click(function(){
+        let username = $('#eusername').val()
+        let password = $('#epassword').val()
+        let roomid = $('#roomid').val()
+
+        if(username == "" || password == "" || roomid == ""){
+            alert("Field cannot be empty")
+        } else {
+            socket.emit('enterroom', {"name": username, "password": password, "id": roomid})
+        }
+    })
+
+    let sendbutton = $('#sendbutton')
+    sendbutton.click(function(){
+        let message = $('#sendmessage').val()
+        if(message == ""){
+            alert("Enter a message")
+        } else {
+            socket.emit('send_msg', {"message": message, id: roomidglobal} )
+        }
+    })
 
 })
--- server.js
+++ server.js
@@ -4,30 +4,42 @@ const http = require('http')
 const socketio = require("socket.io")
 const server = http.Server(app)
 const io = socketio(server)
+const uniqid = require('uniqid');
 
-let users = {}
+let rooms = {}
 
 app.use('/', express.static("./frontend"))
 
 io.on("connection", function(socket){
-    console.log("User connected at " + socket.id)
-    io.emit('connected')
-
-    socket.on('login', function(data){
-        users[data.user] = socket.id;
+    socket.on('createroom', function(data){
+        let id = uniqid()
+        rooms[id] = {"password": data.password, "users": [{"name": data.name, "socketid": socket.id}]}
+        socket.emit('roomcreated', {"id": id, "name": data.name})
     })
 
-    socket.on('send_msg', function(data){
-        if(data.message.startsWith('@')){
-            let recipient = data.message.split(":")[0].substr(1)
-            console.log(users[recipient])
-
-            io.to(users[recipient]).emit('rcv_msg', data)
+    socket.on('enterroom', function(data){
+        if(rooms[data.id] == undefined || rooms[data.id]["password"] != data.password){
+            socket.emit('noroomexists');
         } else {
-            socket.broadcast.emit('rcv_msg', data)
+            rooms[data.id]["users"].push({"name": data.name, "socketid": socket.id})
+            socket.emit('roomcreated', {"id": data.id, "name": data.name})
         }
     })
 
+    socket.on('send_msg', function(data){
+        let username = null;
+        for(let user of rooms[data.id]["users"]){
+            if(user["socketid"] == socket.id){
+                username = user['name']
+            }
+        }
+        
+
+        for(let user of rooms[data.id]["users"]){
+            io.to(user["socketid"]).emit('rcv_msg', {"name": username, "message": data.message})
+        }
+
+    })
 })
 
 server.listen(2000)
